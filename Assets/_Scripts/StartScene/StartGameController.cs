9f45996a539579f003905aa1b480ec7e68bf7dec
RRomkq <ratanin.roman@gmail.com>
2025-04-12 17:12:09 +0300

    logo and webgl build

---
+++ Assets/_Scripts/StartScene/StartGameController.cs
@@ -0,0 +1,28 @@
+﻿// -------------------------------------------------------------------------
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+// Copyright (c) 2019-2024 Gear Games, LTD. All rights reserved.
+// -------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Scripts.StartScene
+{
+    public class StartGameController : MonoBehaviour
+    {
+        [SerializeField] private Button m_startButton;
+        [SerializeField] private GameObject m_releaseDateContainer;
+
+        private void Awake()
+        {
+#if IN_DEVELOPMENT
+            m_releaseDateContainer.SetActive(true);
+            m_startButton.gameObject.SetActive(false);
+#else
+            m_releaseDateContainer.SetActive(false);
+            m_startButton.gameObject.SetActive(true);
+#endif
+        }
+    }
+}
