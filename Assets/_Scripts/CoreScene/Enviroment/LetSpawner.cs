2ad5fea03debe3a3ea9e66a8a147752273584e14
vnekhoroshev <vcitichrt@gmail.com>
2025-04-12 17:03:58 +0300

    генератор лабиринти

--- Assets/_Scripts/CoreScene/Enviroment/LetSpawner.cs
+++ Assets/_Scripts/CoreScene/Enviroment/LetSpawner.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using _Scripts.CoreScene;
 using _Scripts.CoreScene.Enviroment;
 using Cysharp.Threading.Tasks;
@@ -26,20 +26,61 @@ public class LetSpawner: MonoBehaviour
     
     public async UniTask StartSpawnBarricade()
     {
-        if (m_isSpawning == true)
+        for (int i = 0; i < m_levelModel.BarricadesCount; i++)
         {
-            return;
-        }
-        
-        m_isSpawning = true;
-        while (m_isSpawning)
-        {
-            Transform line = m_lineHandler.BarricadeLines[Random.Range(0, m_lineHandler.BarricadeLines.Count)];
-
-            m_instantiator.InstantiatePrefab(barricadePrefab, line.position, quaternion.identity, line.transform);
+            GenerateMaze();
 
             await UniTask.Delay(m_levelModel.SpawnBaricadesDelay * 1000);
         }
+    }
+    
+    void GenerateMaze()
+    {
+        List<int> emptyIndexes = new List<int>(5) {0, 0, 0, 0, 0};
+
+        if (m_levelModel.DifficultyLevel == DifficultyLevel.Easy)
+        {
+            int index = Random.Range(0, m_lineHandler.BarricadeLines.Count);
+            for (int i = 0; i < m_lineHandler.BarricadeLines.Count; i++)
+            {
+                emptyIndexes[i] = i == index ? 1 : 0;
+            }
+        }
+        else if (m_levelModel.DifficultyLevel == DifficultyLevel.Medium)
+        {
+            List<int> numbers = new List<int>() { 0, 1, 2, 3, 4 };
+            int wallCount = Random.Range(2, 4);
+            for (int i = 0; i < wallCount; i++)
+            {
+                int index = Random.Range(0, numbers.Count);
+                emptyIndexes[index] = 1;
+                numbers.Remove(index);
+            }
+
+            foreach (var index in numbers)
+            {
+                emptyIndexes[index] = 0;
+            }
+        }
+        else
+        {
+            int index = Random.Range(0, m_lineHandler.BarricadeLines.Count);
+            for (int i = 0; i < m_lineHandler.BarricadeLines.Count; i++)
+            {
+                emptyIndexes[i] = i == index ? 0 : 1;
+            }
+        }
+
+        for(int i = 0; i < emptyIndexes.Count; i++)
+        {
+            if (emptyIndexes[i] == 1)
+            {
+                Transform line = m_lineHandler.BarricadeLines[i];
+
+                m_instantiator.InstantiatePrefab(barricadePrefab, line.position, quaternion.identity, line.transform);
+            }
+        }
+        
     }
     
     public void StopSpawnBarricade() => m_isSpawning = false;
