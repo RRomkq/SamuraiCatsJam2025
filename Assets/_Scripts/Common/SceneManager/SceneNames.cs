6a8d3276770e55aa970c64487e8fcb83e7e3077c
RRomkq <ratanin.roman@gmail.com>
2025-04-12 16:38:27 +0300

    scenes and project context

---
+++ Assets/_Scripts/Common/SceneManager/SceneNames.cs
@@ -0,0 +1,16 @@
+﻿// -------------------------------------------------------------------------
+// Unauthorized copying of this file, via any medium is strictly prohibited.
+// Proprietary and confidential.
+// Copyright (c) 2019-2024 Gear Games, LTD. All rights reserved.
+// -------------------------------------------------------------------------
+
+namespace _Scripts
+{
+    public class SceneNames
+    {
+        public const string START_SCENE = "StartScene";
+        public const string INTRO_SCENE = "IntroScene";
+        public const string CORE_SCENE = "CoreScene";
+        public const string OUTRO_SCENE = "OutroScene";
+    }
+}
