56365cbef5ba6f1c508259a69a138c4bf37302d8
vnekhoroshev <vcitichrt@gmail.com>
2025-04-13 12:57:45 +0300

    fix

--- Assets/_Scripts/CoreScene/LevelStarter.cs
+++ Assets/_Scripts/CoreScene/LevelStarter.cs
@@ -64,7 +64,7 @@ namespace _Scripts.CoreScene
 
         public void StartLevel()
         {
-            m_levelModel.SetLevelSettings(m_levelsData.LevelSettingsMap[m_levelModel.NeedMoneyForPassenger.FindLastIndex(needMoney => needMoney < m_playerMoneyModel.Money)]);
+            m_levelModel.SetLevelSettings(m_levelsData.LevelSettingsMap[m_levelModel.NeedMoneyForPassenger.FindLastIndex(needMoney => needMoney <= m_playerMoneyModel.Money)]);
             m_playerMoveController.SetBoard((int)m_levelModel.DifficultyLevel);
             
             m_playerMoveController.GoToTargetInstant(StartPlayerPosition);
